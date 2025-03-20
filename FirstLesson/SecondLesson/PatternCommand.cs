5bab31218c4b8a70a22f4ecd03b84bbc1fdb1383
Vladimir-wq-s <midomipar81@gmail.com>
2025-03-20 15:05:42 +0700

    Выполнил второе ДЗ!

---
+++ FirstLesson/SecondLesson/PatternCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+public interface ICommand
+{
+    void Execute();
+}
+
+
+public class TurnOnCommand : ICommand
+{
+    private readonly string _component;
+
+    public TurnOnCommand(string component)
+    {
+        _component = component;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine($"{_component} is turned on.");
+    }
+}
+
+
+public class TurnOffCommand : ICommand
+{
+    private readonly string _component;
+
+    public TurnOffCommand(string component)
+    {
+        _component = component;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine($"{_component} is turned off.");
+    }
+}
+
+
+public class RemoteControl
+{
+    private ICommand _command;
+
+    public void SetCommand(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void PressButton()
+    {
+        _command.Execute();
+    }
+}
---
+++ FirstLesson/SecondLesson/PatternStrategy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public interface ICoolingStrategy
+{
+    void Cool();
+}
+
+
+public class AirCoolingStrategy : ICoolingStrategy
+{
+    public void Cool()
+    {
+        Console.WriteLine("Cooling the CPU with air cooling.");
+    }
+}
+
+
+public class LiquidCoolingStrategy : ICoolingStrategy
+{
+    public void Cool()
+    {
+        Console.WriteLine("Cooling the CPU with liquid cooling.");
+    }
+}
+
+
+public class CPU
+{
+    private ICoolingStrategy _coolingStrategy;
+
+    public void SetCoolingStrategy(ICoolingStrategy coolingStrategy)
+    {
+        _coolingStrategy = coolingStrategy;
+    }
+
+    public void Cool()
+    {
+        _coolingStrategy.Cool();
+    }
+}
